88e73771a6dc699f25c72ede902582da73e15229
Anyaegbu Francis <anyaegbufrancis@gmail.com>
2020-09-03 23:22:55 +0400

    schema update

--- cms_schema.sql
+++ cms_schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE employee (
   employee_id int AUTO_INCREMENT NOT NULL,
   first_name varchar(30) NOT NULL,
   last_name varchar(30) NOT NULL,
+  role_id int NOT NULL,
   manager_name varchar(30) NOT NULL,
   PRIMARY KEY(employee_id)
 );
@@ -17,30 +18,15 @@ CREATE TABLE role (
   role_id int AUTO_INCREMENT NOT NULL,
   job_title varchar(30) NOT NULL,
   salary decimal NOT NULL,
+  department_id int NOT NULL,
   PRIMARY KEY(role_id)
 );
 
-CREATE TABLE manager (
-  manager_id int AUTO_INCREMENT NOT NULL,
-  manager_name varchar(30) NOT NULL,
-  PRIMARY KEY(manager_id)
-);
-
-//Add Foreign Keys to Employee
-ALTER TABLE employee 
-	ADD role_id int NOT NULL;
+--Add Foreign Keys to Employee
 ALTER TABLE employee 
 	ADD CONSTRAINT fk_role_id
-	FOREIGN KEY(role_id) REFERENCES role(role_id);    
-
-ALTER TABLE employee 
-    ADD manager_id int NOT NULL;  
-ALTER TABLE employee 
-	ADD CONSTRAINT fk_manager_id
-	FOREIGN KEY(manager_id) REFERENCES manager(manager_id);
+	FOREIGN KEY(role_id) REFERENCES role(role_id); 
     
-ALTER TABLE role 
-    ADD department_id int NOT NULL;  
 ALTER TABLE role 
 	ADD CONSTRAINT fk_department_id
 	FOREIGN KEY(department_id) REFERENCES department(department_id)
